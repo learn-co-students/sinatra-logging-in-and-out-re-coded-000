60352f7d8e5338509d5d12b5ad96acea2f6c3f18
ALI ALRAWI <ali.developer90@gmail.com>
2016-12-05 21:48:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,22 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+       session[:user_id] = @user.id
+       redirect '/account'
+   else
+     erb :error
+   end
   end
 
   get '/account' do
+    if session[:user_id] != nil
+       @user=Helpers.current_user(session)
+       erb :account
+    else
+      erb :error
+    end
 
   end
 
@@ -24,4 +36,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
